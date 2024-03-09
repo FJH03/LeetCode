5b3b01f133115d31a8291c2c013bb93fd2ed33f2
FJH_03 <fengjiahe5@gmail.com>
2024-03-09 21:40:28 +0800

    完成环形链表问题

---
+++ src/main/java/No_141/Solution.java
@@ -0,0 +1,32 @@
+package No_141;
+
+import common.ListNode;
+
+/**
+ * @Created with Intellij IDEA Ultimate 2022.02.03 正式旗舰版
+ * @Author: 2113042621-冯佳和
+ * @ClassName: Solution
+ * @Date: 2024/3/9
+ * @Time: 18:25
+ * @Description:环形链表
+ */
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode faster = head;
+
+        while (faster.next != null && faster.next.next != null) {
+            slow = slow.next;
+            faster = faster.next.next;
+            if (faster == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
