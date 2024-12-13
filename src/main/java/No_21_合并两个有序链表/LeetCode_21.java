077c665435574654d32e5790affc67b04f6c27b8
FJH_03 <fengjiahe5@gmail.com>
2024-12-13 11:02:30 +0800

    重写了下排序

--- src/main/java/No_21_合并两个有序链表/LeetCode_21.java
+++ src/main/java/No_21_合并两个有序链表/LeetCode_21.java
@@ -10,7 +10,7 @@ import common.ListNode;
  * @Time: 10:22
  * @Description:合并两个有序链表(双指针)
  */
-public class LeetCode_21_1 {
+public class LeetCode_21 {
     public static void main(String[] args) {
         ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
 
@@ -18,5 +18,11 @@ public class LeetCode_21_1 {
         ListNode p = new Solution_1().mergeTwoLists(l1, l2);
 
         p.showValue(p);
+
+        ListNode l3 = new ListNode(2, new ListNode(7, new ListNode(8)));
+
+        ListNode l4 = new ListNode(1, new ListNode(5, new ListNode(6)));
+        ListNode p2 = new Solution_2().mergeTwoLists(l3, l4);
+        p2.showValue(p2);
     }
 }
